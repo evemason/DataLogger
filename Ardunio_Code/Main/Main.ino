c18fe8ef43fdb5dc9c0957f6182a0f572a22bfcc
E.A. Mason <eam86@cam.ac.uk>
2023-05-16 12:44:21 +0100

    Firmware start

--- Ardunio_Code/Main/Main.ino
+++ Ardunio_Code/Main/Main.ino
@@ -1,9 +1,24 @@
+// including all libaries required
+
+#include "Constants.h"
+#include <Wire.h>
+
+//include functions
+extern void Temperature();
+extern void Light();
+extern void Moisture();
+
 void setup() {
   // put your setup code here, to run once:
-
+  // speed at which arduino passes data to the computor
+  Serial.begin(9600);
+  pinMode(Led, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Temperature();
+  Light();
+  Moisture();
+  //Serial.println(String(temp), " ", String(light), " ", String(moisture_voltage));
 }
