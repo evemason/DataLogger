d348799e8b297857b9cfd3f9abb49158d47c8881
E.R. Salter <ers66@cam.ac.uk>
2023-05-25 15:52:59 +0100

    Arduino code for reading Serial communication

--- Ardunio_Code/Main/Main.ino
+++ Ardunio_Code/Main/Main.ino
@@ -9,7 +9,7 @@ extern void Light();
 extern void Moisture();
 extern void Average(int sensor);
 
-
+char incomingByte; // variable to store the serial data in 
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,19 +17,34 @@ void setup() {
   Serial.begin(9600);
   pinMode(Led, OUTPUT);
 
-  //analogReference(INTERNAL);
+  analogReference(INTERNAL);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Temperature();
-  Light();
-  Average(light);
-  Moisture();
+  //Light();
+  Average(temp);
+  //Moisture();
   //Serial.println(String(temp)+ "," + String(light) + "," +String(moisture_voltage));
 
   //temp_voltage = analogRead(temperature_pin);
   //Serial.println(temp);  
   
   //Serial.println(String(light_voltage) + "," + String(light_current)+  "," + String(light)+  "," + String(av));
+  //Serial.println(av);
+  //delay(1000); //collects data every second
+
+  if (Serial.available()>0){
+    // read the imcoming byte 
+    incomingByte = Serial.read();
+    }
+
+  if (incomingByte == 'T'){
+    Serial.println("temp" + String(av));
+    }
+
+  Serial.println(av);
+    
+  
 }
